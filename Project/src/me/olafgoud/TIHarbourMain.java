dd9f4c2d9f18ee158a05d03f918913eb174b94fd
OlafGoud <olafgoud@gmail.com>
2024-05-06 12:33:50 +0200

    added GUI begin

--- Project/src/me/olafgoud/TIHarbourMain.java
+++ Project/src/me/olafgoud/TIHarbourMain.java
@@ -1,9 +1,42 @@
 package me.olafgoud;
 
-public class TIHarbourMain {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import me.olafgoud.utils.screen.ScreenTransform;
 
+public class TIHarbourMain extends Application{
+
+	
 	public static void main(String[] args) {
+		/*try {
+			String system = System.getProperty("os.name");
+			System.out.println(system);
+			if (system.equalsIgnoreCase("Windows 11")) {
+				Runtime.getRuntime().exec("cmd /c start cmd.exe");
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.out.println("Hello World");
+		*/
+        launch(args);
 
 	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(ScreenTransform.createContent(), 300, 300, Color.GRAY));
+        stage.show();
+        stage.setResizable(false);
+        stage.setFullScreenExitHint(null);
+        stage.setTitle("THIS IS A TEST");
+		return;
+	}
+	
+	
 
 }
